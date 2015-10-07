887acea53d14643e22ae88ed75101976076bb5c3
Brandon O'Brien <a-bobrien@expedia.com>
2015-10-06 19:31:20 -0700

    changing requestid to reqid

--- app.js
+++ app.js
@@ -91,25 +91,25 @@ app.get('/create', function(req, res){
 });
 
 app.get('/outreachConfirm', function(req, res){
-    var request = requests[req.query.requestId];
+    var request = requests[req.query.reqId];
     if(request) {
         request.outReachConfirmed = true;
         broadcast();
-        res.json({ reqId: req.query.requestId, msg: "SUCCESS"});
+        res.json({ reqId: req.query.reqId, msg: "SUCCESS"});
     }else{
-        res.json({ reqId: req.query.requestId, msg: "FAILURE"});
+        res.json({ reqId: req.query.reqId, msg: "FAILURE"});
     }
 
 });
 
 app.get('/requesterConfirm', function(req, res){
-    var request = requests[req.query.requestId];
+    var request = requests[req.query.reqId];
     if(request) {
         request.requesterConfirmed = true;
         broadcast();
-        res.json({ reqId: req.query.requestId, msg: "SUCCESS"});
+        res.json({ reqId: req.query.reqId, msg: "SUCCESS"});
     }else{
-        res.json({ reqId: req.query.requestId, msg: "FAILURE"});
+        res.json({ reqId: req.query.reqId, msg: "FAILURE"});
     }
 
 });
